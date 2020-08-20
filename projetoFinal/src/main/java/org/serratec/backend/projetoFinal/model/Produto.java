350ebc38fcdfa0e6a4973c024b71fcda9131b923
brjoaof <brjoaof@gmail.com>
2020-08-20 13:53:30 -0300

    Setup Inicial

---
+++ projetoFinal/src/main/java/org/serratec/backend/projetoFinal/model/Produto.java
@@ -0,0 +1,49 @@
+package org.serratec.backend.projetoFinal.model;
+
+import java.util.Date;
+
+public class Produto {
+	private Integer codigoProduto;
+	private String nome;
+	private String descricao;
+	private Integer quantidadeEstoque;
+	private Date dataFabricacao;
+	private Double valorUnitario;
+	public Integer getCodigoProduto() {
+		return codigoProduto;
+	}
+	public void setCodigoProduto(Integer codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	public Date getDataFabricacao() {
+		return dataFabricacao;
+	}
+	public void setDataFabricacao(Date dataFabricacao) {
+		this.dataFabricacao = dataFabricacao;
+	}
+	public Double getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	
+}
